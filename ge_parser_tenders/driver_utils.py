7451d3940ab204c65cfa7971320619620583040d
goodven152 <goodven152@gmail.com>
2025-06-14 16:11:48 +0400

    fix: восстановлен параметр service при инициализации драйвера Chrome

--- ge_parser_tenders/driver_utils.py
+++ ge_parser_tenders/driver_utils.py
@@ -27,8 +27,8 @@ def make_driver(headless: bool = True, download_dir: Path | None = None) -> webd
     opts.add_experimental_option("prefs", prefs)
 
     # driver_path = ChromeDriverManager().install()
-    service = Service('/usr/bin/chromedriver')
-    driver = webdriver.Chrome(options=opts)
+    # service = Service(driver_path)
+    driver = webdriver.Chrome(options=opts, service=Service("/usr/bin/chromedriver"),  )
     # driver = webdriver.Chrome(service=service, options=opts)
     driver.set_page_load_timeout(60)
     return driver
